08151ca2bafcf9ded219c87b32af1eb043e33d32
Jeff Porter <jeff.porter@acm.org>
2022-05-21 12:35:11 -0700

    Initial Commit

---
+++ SolasApp/SolasAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolasAppApp.swift
+//  SolasApp
+//
+//  Created by Jeffrey Porter on 5/21/22.
+//
+
+import SwiftUI
+
+@main
+struct SolasAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
